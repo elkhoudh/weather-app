fb157f8ad76ae768cc31da2d4a017b2bea75cdc3
Hamza Elkhoudiri <elkhoudh@gmail.com>
2018-10-28 21:44:02 -0400

    Finished weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "0d16f4e30f3a4606a6612312182910";
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.apixu.com/v1/current.json?key=${API_KEY}&q=${city},${country}`
+    );
+    const data = await api_call.json();
+    console.log(data);
+    if (city && country) {
+      this.setState({
+        temperature: data.current.temp_f,
+        city: data.location.name,
+        country: data.location.country,
+        humidity: data.current.humidity,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        error: "Please enter the values."
+      });
+    }
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
