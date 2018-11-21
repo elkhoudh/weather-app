61b16760762ad1e1a1069cca3d55cf09a941a66c
Hamza Elkhoudiri <elkhoudh@gmail.com>
2018-11-21 18:44:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,23 +46,19 @@ class App extends React.Component {
     return (
         <div className="wrapper">
           <div className="main">
-            <div className="container">
-              <div className="row">
-                <div className="col-xs-7 form-container">
-                  <Form getWeather={this.getWeather} />
-                  <Weather
-                    temperature={this.state.temperature}
-                    humidity={this.state.humidity}
-                    city={this.state.city}
-                    country={this.state.country}
-                    description={this.state.description}
-                    error={this.state.error}
-                  />
-                </div>
-                <div className="col-xs-5 title-container">
-                  <Titles />
-                </div>
-              </div>
+            <div className="col-xs-7 form-container">
+              <Form getWeather={this.getWeather} />
+              <Weather
+                temperature={this.state.temperature}
+                humidity={this.state.humidity}
+                city={this.state.city}
+                country={this.state.country}
+                description={this.state.description}
+                error={this.state.error}
+              />
+            </div>
+            <div className="col-xs-5 title-container">
+              <Titles />
             </div>
           </div>
         </div>
